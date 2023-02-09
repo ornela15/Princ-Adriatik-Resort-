d6a0e3f112a22c2ac8c82b739b14a363e5cd1c99
ornela15 <81656546+ornela15@users.noreply.github.com>
2023-02-09 23:47:27 +0100

    Add files via upload

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PRINCI.Startup))]
+namespace PRINCI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
